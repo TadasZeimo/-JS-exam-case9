2925e907e1d0aca480c961d57f38313b333d6a4b
Tadas <tadas.zeimo@gmail.com>
2022-10-27 20:18:19 +0300

    case9 solved

--- script.js
+++ script.js
@@ -6,3 +6,23 @@ title, director, budget
 Metodas: 
 wasExpensive() - jeigu filmo budget bus didesnę nei 100 000 000 mln USD, tada gražins true, kitu atveju false 
 ------------------------------------------------------------------------------------------------------ */
+
+class Movie {
+  constructor(title, director, buget) {
+    this.title = title;
+    this.director = director;
+    this.buget = buget;
+    this.wasExpensive = function () {
+      if (this.buget > 100e6) {
+        return true;
+      } else {
+        return false;
+      }
+    };
+  }
+}
+
+const movieInfo = new Movie("Terminator", "James Cameron", 68000000);
+
+console.log(movieInfo);
+console.log(movieInfo.wasExpensive());
